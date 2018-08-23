ba52df5a3edf52070114bf2da0e0de500e7c2a85
Celien Nanson <cesliens@gmail.com>
2018-08-23 15:22:38 +0200

    Temporary skip yim update as it takes too long

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -4,7 +4,7 @@ sleep 15
 set -xe
 
 # Install all the necessary tools and dependencies
-sudo yum -y update
+#sudo yum -y update
 sudo yum -y install git
 sudo yum -y install ansible
 sudo yum install -y yum-utils
